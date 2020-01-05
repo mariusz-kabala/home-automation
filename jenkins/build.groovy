85859c57be82fe6e1a54783b63aaa32669e7289a
Mariusz <mariusz@kabala.waw.pl>
2020-01-05 17:57:42 +0100

    ci: fix build job

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -63,7 +63,7 @@ pipeline {
                     def packagesList = packages.split(',')
                     packagesList.each{
                         def props = readJSON file: "packages/${it}/package.json"
-                        def currentApp = docker.build(props['name'].replace('@', '').toLowerCase(), "-f packages/${it}/Dockerfile .")
+                        def currentApp = docker.build(props['name'].replace('@', '').replace('-', '').toLowerCase(), "-f packages/${it}/Dockerfile .")
                         docker.withRegistry('https://docker-registry.kabala.tech', 'docker-registry-credentials') {
                             currentApp.push("v${props['version']}")
                         }
