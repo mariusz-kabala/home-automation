401248b0f56cd2ac63f9dbb28c707d59fa738e9e
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-01-13 16:53:37 +0100

    feat: add redisClient package

---
+++ packages/redisClient/src/index.ts
@@ -0,0 +1,19 @@
+import { Promise } from 'bluebird'
+import * as redis from 'redis'
+import { logger } from '@home/logger'
+import config from 'config'
+
+Promise.promisifyAll(redis.RedisClient.prototype)
+Promise.promisifyAll(redis.Multi.prototype)
+
+export const redisClient = redis.createClient({
+  host: config.get<string>('redisHost'),
+  port: config.get<number>('redisPort'),
+})
+
+redisClient.on('error', err =>
+  logger.log({
+    level: 'error',
+    message: `Redis error ${err}`,
+  }),
+)
