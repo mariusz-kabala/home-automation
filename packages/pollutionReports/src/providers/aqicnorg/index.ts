37928ebef802a15bcdeee0edbd7281eb7676af67
Mariusz <mariusz@kabala.waw.pl>
2020-02-01 13:13:51 +0100

    feat: support new services in stats collector

--- packages/pollutionReports/src/providers/aqicnorg/index.ts
+++ packages/pollutionReports/src/providers/aqicnorg/index.ts
@@ -6,11 +6,7 @@ import { publish } from '@home/mqtt'
 import { logger } from '@home/logger'
 
 async function fetchResults(location: string) {
-  const request = await fetch(
-    `https://api.waqi.info/feed/${location}/?token=${config.get<string>(
-      'aqicnorgAPIKey',
-    )}`,
-  )
+  const request = await fetch(`https://api.waqi.info/feed/${location}/?token=${config.get<string>('aqicnorgAPIKey')}`)
 
   if (!request.ok) {
     throw request
@@ -22,41 +18,41 @@ async function fetchResults(location: string) {
 }
 
 function publishResults(response: IAqicnorgResponse, location: string) {
-    publish(`aqicnorg/${location}`, {
-        aqi: response.data.aqi,
-        idx: response.data.idx,
-        iaqi: {
-            co: response.data.iaqi.co.v,
-            no2: response.data.iaqi.no2.v,
-            pm25: response.data.iaqi.pm25.v,
-            so2: response.data.iaqi.so2.v,
-            t: response.data.iaqi.t.v,
-        },
-        traceid: uuid4()
-    })
+  publish(`aqicnorg/${location}`, {
+    aqi: response.data.aqi,
+    idx: response.data.idx,
+    iaqi: {
+      co: response.data.iaqi.co.v,
+      no2: response.data.iaqi.no2.v,
+      pm25: response.data.iaqi.pm25.v,
+      so2: response.data.iaqi.so2.v,
+      t: response.data.iaqi.t.v,
+    },
+    traceid: uuid4(),
+  })
 }
 
 export async function runAqicnorg() {
-    const locations = config.get<string[]>('aqicnorgLocations')
+  const locations = config.get<string[]>('aqicnorgLocations')
 
-    for (const location of locations) {
-        try {
-            const results = await fetchResults(location)
+  for (const location of locations) {
+    try {
+      const results = await fetchResults(location)
 
-            if (results.status !== 'ok') {
-                logger.log({
-                    level: 'error',
-                    message: `Invalid Aqicnorg response ${JSON.stringify(results)}`,
-                  })
-                  continue
-            }
+      if (results.status !== 'ok') {
+        logger.log({
+          level: 'error',
+          message: `Invalid Aqicnorg response ${JSON.stringify(results)}`,
+        })
+        continue
+      }
 
-            publishResults(results, location)
-        } catch (err) {
-            logger.log({
-                level: 'error',
-                message: err,
-            })
-        }
+      publishResults(results, location)
+    } catch (err) {
+      logger.log({
+        level: 'error',
+        message: err,
+      })
     }
+  }
 }
--- packages/statsCollector/src/index.ts
+++ packages/statsCollector/src/index.ts
@@ -1,95 +1,21 @@
-import { subscribe } from '@home/mqtt'
 import { logger } from '@home/logger'
 
-import { createDBIfNeeded, influx } from './clients/db'
-
-const SUPPORTED_TYPES = ['ZHALightLevel', 'ZHATemperature', 'ZHAPresence', 'ZHASwitch', 'ZHAHumidity', 'ZHAPressure']
+import { createDBIfNeeded } from './clients/db'
+import { subscribeForPollutionReports } from './subscriptions/pollutionReports'
+import { subscribeForOpenWeatherReports } from './subscriptions/openweather'
+import { subscribeForPlugReports } from './subscriptions/tuyaPlugs'
 
 async function start() {
   await createDBIfNeeded('home')
 
-  subscribe('sensors/zigbee/#', async (msg: any) => {
-    const { type, state, id, name } = msg
-
-    if (SUPPORTED_TYPES.includes(type) && state) {
-      try {
-        await influx.writePoints([
-          {
-            measurement: type,
-            tags: { deviceId: id, deviceName: name },
-            fields: state,
-          },
-        ])
-        logger.info({
-          level: 'info',
-          traceid: msg.traceid,
-          message: `Saving new measurement ${JSON.stringify(state)}`,
-          deviceId: id,
-          deviceName: name,
-          measurement: type,
-        })
-      } catch (err) {
-        logger.log({
-          level: 'error',
-          message: err,
-        })
-      }
-    }
+  logger.log({
+    level: 'info',
+    message: 'Application started',
   })
 
-  subscribe('forecast/#', async (msg: any) => {
-    const {
-      name,
-      id,
-      visibility,
-      main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
-      wind: { speed, deg },
-      clouds: { all },
-    } = msg
-    try {
-      await influx.writePoints([
-        {
-          measurement: 'forecast',
-          tags: { cityName: name, cityId: id },
-          fields: {
-            visibility,
-            temp,
-            feels_like,
-            temp_min,
-            temp_max,
-            pressure,
-            humidity,
-            windSpeed: speed,
-            windDeg: deg,
-            clouds: all,
-          },
-        },
-      ])
-      logger.info({
-        level: 'info',
-        traceid: msg.traceid,
-        message: `Saving new forecast ${JSON.stringify({
-          visibility,
-          temp,
-          feels_like,
-          temp_min,
-          temp_max,
-          pressure,
-          humidity,
-          windSpeed: speed,
-          windDeg: deg,
-          clouds: all,
-        })}`,
-        cityName: name,
-        cityId: id,
-      })
-    } catch (err) {
-      logger.log({
-        level: 'error',
-        message: err,
-      })
-    }
-  })
+  subscribeForPollutionReports()
+  subscribeForOpenWeatherReports()
+  subscribeForPlugReports()
 }
 
 start()
