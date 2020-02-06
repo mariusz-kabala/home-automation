b0631f0c249c5b391a67336bf21ae372ac051596
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-02-06 08:41:40 +0100

    test: unit tests for deviceDiscovery service

---
+++ packages/deviceDiscovery/src/index.test.ts
@@ -0,0 +1,105 @@
+import { subscribe, publish, getSubscriptions } from '@home/mqtt'
+// import isReachable from 'is-reachable'
+// import ping from 'ping'
+
+jest.useFakeTimers()
+
+require('./index')
+
+jest.mock('is-reachable', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+jest.mock('ping', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+jest.mock('config', () => ({
+  __esModule: true,
+  default: {
+    get: jest.fn((key: string) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const fakeConfig: any = {
+        mqttHost: 'fake-mqtt-host',
+        mqttPort: 'fake-mqtt-port',
+        mqttPrefix: 'home',
+        devices: [
+          {
+            name: 'mariusz-phone',
+            address: '192.168.0.157',
+            checkInterval: 180000, // 3min
+          },
+          {
+            name: 'weronika-phone',
+            address: '192.168.0.192',
+            mac: '48:BF:6B:09:66:B2',
+            checkInterval: 180000, // 3min
+          },
+          {
+            name: 'fake-device',
+            address: '192.168.0.10',
+            mac: 'BB:BB:BB:BB:BB:BB',
+            checkInterval: 180000, // 3min
+          },
+        ],
+      }
+
+      return fakeConfig[key] || ''
+    }),
+  },
+}))
+
+describe('Device Discovery service', () => {
+  afterAll(() => {
+    jest.clearAllTimers()
+  })
+
+  it('Should subscribe to proper mqtt topics', () => {
+    expect(subscribe).toBeCalledTimes(2)
+
+    expect(subscribe).toHaveBeenNthCalledWith(1, 'devices/+/checkStatus', expect.any(Function))
+    expect(subscribe).toHaveBeenNthCalledWith(2, 'openwrt/clients', expect.any(Function))
+  })
+
+  it('Should setup intervals to check device availability', () => {
+    expect(setInterval).toHaveBeenCalledTimes(3) // one time per device defined in the config
+  })
+
+  it('Should return proper status of device connected to openwrt', async () => {
+    const subscriptions = getSubscriptions()
+    const [clientsCallback] = subscriptions['openwrt/clients']
+    const [checkStatusCallback] = subscriptions['devices/+/checkStatus']
+
+    expect(clientsCallback).toBeInstanceOf(Function)
+
+    clientsCallback(['48:BF:6B:09:66:B2'])
+
+    await checkStatusCallback(null, 'devices/weronika-phone/checkStatus')
+
+    expect(publish).toBeCalledTimes(1)
+
+    expect(publish).toHaveBeenNthCalledWith(
+      1,
+      'devices/weronika-phone/status',
+      {
+        isReachable: true,
+      },
+      { qos: 1, retain: true },
+    )
+
+    await checkStatusCallback(null, 'devices/fake-device/checkStatus')
+
+    expect(publish).toBeCalledTimes(2)
+
+    expect(publish).toHaveBeenNthCalledWith(
+      2,
+      'devices/fake-device/status',
+      {
+        isReachable: false,
+      },
+      { qos: 1, retain: true },
+    )
+  })
+})
--- setupTests.ts
+++ setupTests.ts
@@ -6,3 +6,28 @@ jest.mock('config', () => ({
     }),
   },
 }))
+
+jest.mock('@home/logger', () => ({
+  logger: {
+    log: jest.fn(),
+  },
+}))
+
+// eslint-disable-next-line no-var
+var subscriptions: { [key: string]: Function[] }
+
+jest.mock('@home/mqtt', () => ({
+  getSubscriptions: () => subscriptions,
+  subscribe: jest.fn().mockImplementation((topic: string, callback: Function) => {
+    if (subscriptions === undefined) {
+      subscriptions = {}
+    }
+
+    if (!Array.isArray(subscriptions[topic])) {
+      subscriptions[topic] = []
+    }
+
+    subscriptions[topic].push(callback)
+  }),
+  publish: jest.fn(),
+}))
