b7f556c7a541b786e67fe0fcd02e2d0ab92e23f2
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-11-22 13:12:50 +0100

    fix: jenkins job fixes

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -1,4 +1,4 @@
-def branch = '';
+def branch = ''
 
 pipeline {
     agent any
@@ -6,7 +6,7 @@ pipeline {
     environment {
         app = ''
         CI = 'true'
-        GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
+        GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no'
         GH_TOKEN = credentials('jenkins-github-accesstoken')
     }
 
@@ -14,7 +14,7 @@ pipeline {
         stage ('prepare') {
             steps {
                 script {
-                    sh "printenv"
+                    sh 'printenv'
                     try {
                         branch = env.GIT_LOCAL_BRANCH
                         branch = branch ?: env.GIT_BRANCH
@@ -23,14 +23,14 @@ pipeline {
                         }
                         branch = branch ?: env.ghprbActualCommit
                     } catch (e) {
-                        println "GIT BRANCH not detected"
+                        println 'GIT BRANCH not detected'
                     }
 
                     sh 'git config user.name "jenkins-kabala.tech"'
                     sh 'git config user.email "jenkins@kabala.tech"'
 
                     if (!branch) {
-                        error "GIT branch to process not found"
+                        error 'GIT branch to process not found'
                     }
 
                     if (branch.startsWith('origin/')) {
@@ -38,7 +38,7 @@ pipeline {
                     }
 
                     println "GIT branch to process: ${branch}"
-                    manager.addShortText(branch, "white", "navy", "1px", "navy")
+                    manager.addShortText(branch, 'white', 'navy', '1px', 'navy')
                 }
             }
         }
@@ -47,21 +47,21 @@ pipeline {
                     checkout([
                             $class                           : 'GitSCM',
                             branches                         : [[name: "${branch}"]],
-                            browser                          : [$class: 'GithubWeb', repoUrl: "https://github.com/mariusz-kabala/homeAutomation"],
+                            browser                          : [$class: 'GithubWeb', repoUrl: 'https://github.com/mariusz-kabala/homeAutomation'],
                             doGenerateSubmoduleConfigurations: false,
                             userRemoteConfigs                : [[
                                 credentialsId: 'github',
                                 refspec      : '+refs/pull/*:refs/remotes/origin/pr/*',
-                                url          : "git@github.com:mariusz-kabala/homeAutomation.git"
+                                url          : 'git@github.com:mariusz-kabala/homeAutomation.git'
                             ]]
                     ])
             }
         }
         stage ('Build Containers') {
-             steps {
+            steps {
                 script {
                     def packagesList = packages.split(',')
-                    packagesList.each{
+                    packagesList.each {
                         def props = readJSON file: "packages/${it}/package.json"
                         def currentApp = docker.build(props['name'].replace('@', '').replace('-', '').toLowerCase(), "-f packages/${it}/Dockerfile .")
                         docker.withRegistry('https://rg.nl-ams.scw.cloud/kabalatech', 'docker-registry-scaleway') {
@@ -78,10 +78,10 @@ pipeline {
             steps {
                 script {
                         def packagesList = packages.split(',')
-                        packagesList.each{
+                        packagesList.each {
                             def props = readJSON file: "packages/${it}/package.json"
 
-                            build job: '(HomeAutomation) Deploy', wait: false, parameters: [
+                            build job: 'home_automation/deploy', wait: false, parameters: [
                                 string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
                                 string(name: 'package', value: "${it}"),
                                 string(name: 'version', value: "v${props.version}"),
