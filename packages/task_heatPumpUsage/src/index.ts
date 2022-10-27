9ddc9598d19bd186f762f5880fd456f76d00ef2d
Mariusz Kabała <mariusz@kabala.waw.pl>
2022-10-27 17:58:36 +0200

    fix: adjust params

--- packages/task_heatPumpUsage/src/index.ts
+++ packages/task_heatPumpUsage/src/index.ts
@@ -7,6 +7,18 @@ const APP_VERSION = '1.25.0.1'
 
 const DEVICE_ID = 28328216
 
+const { MONTH, YEAR } = process.env
+
+function getDaysInMonth(year: number, month: number) {
+  return new Date(year, month, 0).getDate()
+}
+
+function getLastDayOfMonth(year: number, month: number) {
+  const days = getDaysInMonth(year, month)
+
+  return days < 10 ? `0${days}` : `${days}`
+}
+
 async function login() {
   const response = await fetch(`${CLOUD_URL}/Login/ClientLogin`, {
     method: 'POST',
@@ -18,7 +30,7 @@ async function login() {
       Password: config.get<string>('heatPumpPassword'),
       Language: 0,
       AppVersion: APP_VERSION,
-      Persist: true,
+      Persist: false,
       CaptchaResponse: null,
     }),
   }).then(res => res.json())
@@ -49,9 +61,23 @@ async function energyReport(token: string, from: string, to: string) {
 }
 
 async function run() {
+  if (!MONTH || MONTH == '') {
+    console.error(`MONTH param (${MONTH}) is invalid`)
+    process.exit(1)
+  }
+
+  if (!YEAR || YEAR == '') {
+    console.error(`YEAR param (${YEAR}) is invalid`)
+    process.exit(1)
+  }
+
   const token = await login()
 
-  energyReport(token, '2022-07-24T00:00:00', '2022-10-25T00:00:00')
+  energyReport(
+    token,
+    `${YEAR}-${MONTH}-01T00:00:00`,
+    `${YEAR}-${MONTH}-${getLastDayOfMonth(parseInt(YEAR, 10), parseInt(MONTH, 10))}T00:00:00`,
+  )
 }
 
 run()
