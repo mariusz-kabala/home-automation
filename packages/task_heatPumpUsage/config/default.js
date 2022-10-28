210a3bab1b8dac850ceaca986e92c2d2b7e1f391
Mariusz Kabała <mariusz@kabala.tech>
2022-10-28 06:25:01 +0200

    fix: working version of heat pump usage task

--- packages/task_heatPumpUsage/config/default.js
+++ packages/task_heatPumpUsage/config/default.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const {
-  MONGO_CONNECTION_STR,
   STATS_DB_HOST,
   STATS_DB_PORT,
   STATS_DB_TOKEN,
@@ -13,14 +12,13 @@ const {
 } = process.env
 
 module.exports = {
-    mongoConnectionStr: MONGO_CONNECTION_STR,
-    heatPumpUsername: HEAT_PUMP_USERNAME,
-    heatPumpPassword: HEAT_PUMP_PASSWORD,
-    statsDb: {
-      host: STATS_DB_HOST,
-      port: STATS_DB_PORT,
-      token: STATS_DB_TOKEN,
-      organisation: STATS_DB_ORGANISATION,
-      bucket: STATS_DB_BUCKET,
-    },
-  }
+  heatPumpUsername: HEAT_PUMP_USERNAME,
+  heatPumpPassword: HEAT_PUMP_PASSWORD,
+  statsDb: {
+    host: STATS_DB_HOST,
+    port: STATS_DB_PORT,
+    token: STATS_DB_TOKEN,
+    organisation: STATS_DB_ORGANISATION,
+    bucket: STATS_DB_BUCKET,
+  },
+}
--- packages/task_heatPumpUsage/src/index.ts
+++ packages/task_heatPumpUsage/src/index.ts
@@ -1,5 +1,7 @@
 import config from 'config'
 import fetch from 'node-fetch'
+import { Point } from '@influxdata/influxdb-client'
+import { InfluxDB } from '@influxdata/influxdb-client'
 
 const CLOUD_URL = 'https://app.melcloud.com/Mitsubishi.Wifi.Client'
 
@@ -9,6 +11,14 @@ const DEVICE_ID = 28328216
 
 const { MONTH, YEAR } = process.env
 
+interface IReportResponse {
+  HotWater: number[]
+  Heating: number[]
+  ProducedHotWater: number[]
+  ProducedHeating: number[]
+  Labels: number[]
+}
+
 function getDaysInMonth(year: number, month: number) {
   return new Date(year, month, 0).getDate()
 }
@@ -17,6 +27,15 @@ function getLastDayOfMonth(year: number, month: number) {
   const days = getDaysInMonth(year, month)
 
   return days < 10 ? `0${days}` : `${days}`
+}
+
+function getInfluxDbWriteAPI() {
+  const influxDB = new InfluxDB({
+    url: `http://${config.get<string>('statsDb.host')}:${config.get<number>('statsDb.port')}`,
+    token: config.get<string>('statsDb.token'),
+  })
+
+  return influxDB.getWriteApi(config.get<string>('statsDb.organisation'), config.get<string>('statsDb.bucket'), 'ms')
 }
 
 async function login() {
@@ -42,8 +61,8 @@ async function login() {
   return ContextKey
 }
 
-async function energyReport(token: string, from: string, to: string) {
-  const response = await fetch(`${CLOUD_URL}/EnergyCost/Report`, {
+async function energyReport(token: string, from: string, to: string): Promise<IReportResponse> {
+  return await fetch(`${CLOUD_URL}/EnergyCost/Report`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -56,8 +75,6 @@ async function energyReport(token: string, from: string, to: string) {
       UseCurrency: false,
     }),
   }).then(res => res.json())
-
-  console.log(response)
 }
 
 async function run() {
@@ -73,11 +90,46 @@ async function run() {
 
   const token = await login()
 
-  energyReport(
+  const report = await energyReport(
     token,
     `${YEAR}-${MONTH}-01T00:00:00`,
     `${YEAR}-${MONTH}-${getLastDayOfMonth(parseInt(YEAR, 10), parseInt(MONTH, 10))}T00:00:00`,
   )
+
+  const writeAPI = getInfluxDbWriteAPI()
+
+  for (let x = 0; x < report.Labels.length; x += 1) {
+    const water = report.HotWater[x]
+    const heating = report.Heating[x]
+    const producedWater = report.ProducedHotWater[x]
+    const producedHeating = report.ProducedHeating[x]
+    const day = report.Labels[x]
+
+    const point = new Point('energy')
+      .tag('deviceName', 'heatPump')
+      .floatField('water', water)
+      .floatField('heating', heating)
+      .floatField('producedWater', producedWater)
+      .floatField('producedHeating', producedHeating)
+      .timestamp(new Date(`${MONTH} ${day} ${YEAR}`))
+
+    console.info(`DATE: ${day} - ${MONTH} - ${YEAR}`)
+    console.info(`water: ${water} kWh`)
+    console.info(`heating: ${heating} kWh`)
+    console.info(`producedWater: ${producedWater} kWh`)
+    console.info(`producedHeating: ${producedHeating} kWh`)
+    console.info('---')
+    writeAPI.writePoint(point)
+  }
+
+  try {
+    await writeAPI.flush()
+    console.info('done')
+    process.exit(0)
+  } catch (err) {
+    console.error('Db write failed', err)
+    process.exit(1)
+  }
 }
 
 run()
